1b41444cc88eddb691809d91db762dad421754da
Philip Taron <philip.taron@gmail.com>
2023-09-01 09:23:27 -0700

    integrate github:nix-systems/x86_64-linux and github:numtide/flake-utils without using either yet

--- flake.nix
+++ flake.nix
@@ -2,32 +2,37 @@
   description = "Philip Taron's configuration(s)";
 
   inputs = {
+    systems.url = "github:nix-systems/x86_64-linux";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
     nixpkgs.url = "github:NixOS/nixpkgs";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, agenix }@inputs: let
-    hostname = "zebul";
-    system = "x86_64-linux";
-  in {
-    formatter."${system}" = nixpkgs.legacyPackages."${system}".nixpkgs-fmt;
-    nixosConfigurations."${hostname}" = nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
-      modules = [
-        { networking.hostName = hostname; }
-        { nixpkgs.hostPlatform = system; }
-        { nixpkgs.config.allowUnfree = true; }
-        { system.stateVersion = "23.05"; }
-        ./boot.nix
-        ./containers.nix
-        ./gui.nix
-        ./hardware.nix
-        ./nix.nix
-        ./programs.nix
-        agenix.nixosModules.default
-      ];
+  outputs = { self, systems, flake-utils, nixpkgs, agenix }@inputs:
+    let
+      hostname = "zebul";
+      system = "x86_64-linux";
+    in
+    {
+      formatter."${system}" = nixpkgs.legacyPackages."${system}".nixpkgs-fmt;
+      nixosConfigurations."${hostname}" = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          { networking.hostName = hostname; }
+          { nixpkgs.hostPlatform = system; }
+          { nixpkgs.config.allowUnfree = true; }
+          { system.stateVersion = "23.05"; }
+          ./boot.nix
+          ./containers.nix
+          ./gui.nix
+          ./hardware.nix
+          ./nix.nix
+          ./programs.nix
+          agenix.nixosModules.default
+        ];
+      };
     };
-  };
 }
