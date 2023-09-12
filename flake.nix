02c7434cf55ded64d144ea7b9021d8f870736831
Philip Taron <philip.taron@gmail.com>
2023-09-12 07:57:03 -0700

    try to remove more darwin inputs

--- flake.nix
+++ flake.nix
@@ -2,13 +2,16 @@
   description = "Philip Taron's configuration(s)";
 
   inputs = {
-    flake-compat.url = "github:nix-community/flake-compat";
+    empty.url = "github:nix-community/flake-compat";
     systems.url = "github:nix-systems/x86_64-linux";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.systems.follows = "systems";
     nixpkgs.url = "github:NixOS/nixpkgs";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Stub these out so that they don't do anything.
+    agenix.inputs.darwin.follows = "empty";
   };
 
   outputs = { self, systems, flake-utils, nixpkgs, agenix, ... }@inputs:
