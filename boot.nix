60587b9cd73769ac2b82ce8e669661ae34602b92
Philip Taron <philip.taron@gmail.com>
2023-09-26 16:13:32 -0700

    add nvidia to initrd

--- boot.nix
+++ boot.nix
@@ -15,7 +15,7 @@
   };
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "nvidia" ];
   boot.extraModulePackages = [ ];
   boot.swraid.enable = false;
 
