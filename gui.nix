f535566519d6e103989ff3846cbf99def748c963
Philip Taron <philip.taron@gmail.com>
2023-09-29 13:58:11 -0700

    Revert "Let's try Hyprland"
    This reverts commit 6d2212978e003768ea5d6b26fd1602ca8a2cf17f.

--- gui.nix
+++ gui.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, ... }:
 
 {
-  programs.hyprland.enable = true;
+  programs.sway.enable = true;
   # services.xserver = {
   #   enable = true;
   #   updateDbusEnvironment = true;
