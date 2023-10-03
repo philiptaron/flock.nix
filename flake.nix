a4ba033c4a8b31ad199265a59e5c4d8667328e80
Philip Taron <philip.taron@gmail.com>
2023-10-03 09:12:06 -0700

    Move overlays to their own file

--- flake.nix
+++ flake.nix
@@ -25,26 +25,8 @@
       system = "x86_64-linux";
     in
     {
-      overlays.default = final: prev: {
-        # Enable building Evolution Data Server without Gnome Online Accounts (GOA)
-        evolution-data-server = prev.evolution-data-server.overrideAttrs (prevAttrs: {
-          buildInputs = builtins.filter (e: e != prev.gnome-online-accounts) prevAttrs.buildInputs;
-          cmakeFlags = ["-DENABLE_GOA=OFF"] ++ prevAttrs.cmakeFlags;
-        });
+      overlays.default = import ./overlays.nix;
 
-        ibus = prev.ibus.overrideAttrs (prevAttrs: {
-          buildInputs = builtins.filter (e: e != prev.gtk2) prevAttrs.buildInputs;
-          configureFlags = ["--disable-gtk2"] ++ prevAttrs.configureFlags;
-        });
-
-        gnome = prev.gnome.overrideScope' (gnome-final: gnome-prev: {
-          # Enable building Gnome Control Center without Gnome Online Accounts (GOA)
-          gnome-control-center = gnome-prev.gnome-control-center.overrideAttrs (prevAttrs: {
-            buildInputs = builtins.filter (e: e != prev.gnome-online-accounts) prevAttrs.buildInputs;
-            patches = prevAttrs.patches ++ [./remove-online-accounts-from-gnome-control-center.patch];
-          });
-        });
-      };
       formatter."${system}" = nixpkgs.legacyPackages."${system}".nixpkgs-fmt;
       nixosModules = {
         programs.agenix = agenix.nixosModules.default;
---
+++ overlays.nix
@@ -0,0 +1,20 @@
+final: prev:
+
+{
+  # Enable building Evolution Data Server without Gnome Online Accounts (GOA)
+  evolution-data-server = prev.evolution-data-server.overrideAttrs (prevAttrs: {
+    buildInputs = builtins.filter (e: e != prev.gnome-online-accounts) prevAttrs.buildInputs;
+    cmakeFlags = [ "-DENABLE_GOA=OFF" ] ++ prevAttrs.cmakeFlags;
+  });
+  ibus = prev.ibus.overrideAttrs (prevAttrs: {
+    buildInputs = builtins.filter (e: e != prev.gtk2) prevAttrs.buildInputs;
+    configureFlags = [ "--disable-gtk2" ] ++ prevAttrs.configureFlags;
+  });
+  gnome = prev.gnome.overrideScope' (gnome-final: gnome-prev: {
+    # Enable building Gnome Control Center without Gnome Online Accounts (GOA)
+    gnome-control-center = gnome-prev.gnome-control-center.overrideAttrs (prevAttrs: {
+      buildInputs = builtins.filter (e: e != prev.gnome-online-accounts) prevAttrs.buildInputs;
+      patches = prevAttrs.patches ++ [ ./remove-online-accounts-from-gnome-control-center.patch ];
+    });
+  });
+}
