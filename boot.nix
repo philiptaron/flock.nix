222d575dba033bd2e1e67cca8b2e3906ef822d20
Philip Taron <philip.taron@gmail.com>
2023-10-09 06:49:55 -0700

    Add in TPM2 tools to the environment

--- boot.nix
+++ boot.nix
@@ -27,6 +27,7 @@
   security.tpm2.enable = true;
   security.tpm2.pkcs11.enable = true;
   security.tpm2.tctiEnvironment.enable = true;
+  environment.systemPackages = [ pkgs.tpm2-tools ];
 
   fileSystems = {
     "/" = {
