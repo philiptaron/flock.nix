ede9fa50246ad88071990ffed9b2c8adb18cf0c5
Philip Taron <philip.taron@gmail.com>
2023-10-17 16:02:51 -0700

    Test out https://github.com/NixOS/nixpkgs/pull/261675

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     empty.url = "path:./empty.nix";
     empty.flake = false;
     systems.url = "github:nix-systems/x86_64-linux";
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:philiptaron/nixpkgs";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
     fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*.tar.gz";
