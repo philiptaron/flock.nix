69bd4300042be38d53c6521f830030986ea74b1e
Philip Taron <philip.taron@gmail.com>
2023-10-30 15:36:06 -0700

    restore frogmouth

--- overlays.nix
+++ overlays.nix
@@ -6,12 +6,6 @@ final: prev:
     buildInputs = builtins.filter (e: e != prev.gnome-online-accounts) prevAttrs.buildInputs;
     cmakeFlags = [ "-DENABLE_GOA=OFF" ] ++ prevAttrs.cmakeFlags;
   });
-  # Until https://github.com/NixOS/nixpkgs/pull/263241 is pulled, imitate it on tip
-  frogmouth = prev.frogmouth.overrideAttrs (prevAttrs: {
-    postUnpack = ''
-      sed -i -e "s,from xdg import,from xdg_base_dirs import," $sourceRoot/frogmouth/data/{config,data_directory}.py
-    '';
-  });
   ibus = prev.ibus.overrideAttrs (prevAttrs: {
     buildInputs = builtins.filter (e: e != prev.gtk2) prevAttrs.buildInputs;
     configureFlags = [ "--disable-gtk2" ] ++ prevAttrs.configureFlags;
