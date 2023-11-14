2a253472c1386f503717f12da4dd417d07ed5d57
Philip Taron <philip.taron@gmail.com>
2023-11-13 16:17:28 -0800

    Add `nix-index` and `nix-tree`

--- nix.nix
+++ nix.nix
@@ -8,4 +8,13 @@
       experimental-features = nix-command flakes
     '';
   };
+  environment.systemPackages = with pkgs; [
+    # Interactively browse a Nix store paths dependencies
+    # https://hackage.haskell.org/package/nix-tree
+    nix-tree
+
+    # A files database for nixpkgs
+    # https://github.com/nix-community/nix-index
+    nix-index
+  ];
 }
