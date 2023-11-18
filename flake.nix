ef09f8280b12363d1f4d1c1fb38def90e8d9434a
Philip Taron <philip.taron@gmail.com>
2023-11-18 09:17:46 -0800

    Flake update (back to github:nixos/nixpkgs)
    • Updated input 'llama-cpp':
        'github:ggerganov/llama.cpp/bbecf3f415797f812893947998bda4f866fa900e' (2023-11-17)
      → 'github:ggerganov/llama.cpp/2923f17f6fec049a71186636c3c4d96408856194' (2023-11-18)
    • Updated input 'nixpkgs':
        'github:philiptaron/nixpkgs/55365f1d34fc87ce2c72ce5e1f0a2f6bf39d2d8f' (2023-11-17)
      → 'github:nixos/nixpkgs/d581fb17d72e918ebe1a30f926ef24e74f776aca' (2023-11-18)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Philip Taron's configuration(s)";
 
   inputs = {
-    nixpkgs.url = "github:philiptaron/nixpkgs/authenticator-rs";
+    nixpkgs.url = "github:nixos/nixpkgs";
     empty.url = "path:./empty.nix";
     empty.flake = false;
     systems.url = "github:nix-systems/x86_64-linux";
