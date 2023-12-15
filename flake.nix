b447324ceaaa67da45beb85185e499794ae03a9c
Philip Taron <philip.taron@gmail.com>
2023-12-15 08:54:31 -0800

    Simplify the flake definition to remove formatter (which I just did by hand) and to let flake-utils be

--- flake.nix
+++ flake.nix
@@ -3,38 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+
+    # Empty flake for making complex flake dependencies stop dead.
     empty.url = "path:./empty.nix";
     empty.flake = false;
 
-    systems.url = "github:nix-systems/x86_64-linux";
-
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.inputs.darwin.follows = "empty";
+    agenix.inputs.home-manager.follows = "empty";
 
     fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*.tar.gz";
     fh.inputs.nixpkgs.follows = "nixpkgs";
 
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.systems.follows = "systems";
-
     llama-cpp.url = "github:ggerganov/llama.cpp";
     llama-cpp.inputs.nixpkgs.follows = "nixpkgs";
-    llama-cpp.inputs.flake-utils.follows = "flake-utils";
 
     nurl.url = "github:nix-community/nurl";
     nurl.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Stub these out so that they don't do anything.
-    agenix.inputs.darwin.follows = "empty";
-    agenix.inputs.home-manager.follows = "empty";
   };
 
-  outputs = inputs@{self, nixpkgs, agenix, systems, ...}: let
-    eachSystem = nixpkgs.lib.genAttrs (import systems);
-    x86_64-linux = builtins.elemAt (import systems) 0;
-  in {
-    formatter = eachSystem (system: nixpkgs.legacyPackages."${system}".nixpkgs-fmt);
-
+  outputs = inputs@{self, nixpkgs, ...}: {
     overlays = {
       default = import ./overlays.nix;
     };
@@ -51,13 +40,13 @@
     };
 
     nixosConfigurations.zebul = nixpkgs.lib.nixosSystem {
-      system = x86_64-linux;
+      system = "x86_64-linux";
       specialArgs = {
         inherit inputs;
       };
       modules = with self.nixosModules; [
         { networking.hostName = "zebul"; }
-        { nixpkgs.hostPlatform = x86_64-linux; }
+        { nixpkgs.hostPlatform = "x86_64-linux"; }
         { nixpkgs.config.allowUnfree = true; }
         { system.stateVersion = "23.05"; }
         ./boot.nix
