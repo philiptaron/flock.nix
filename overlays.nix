edf0f8aa42c331dda4e3e726f35e92fd2be08112
Philip Taron <philip.taron@gmail.com>
2023-12-15 15:14:31 -0800

    Remove gnome-online-accounts from gvfs

--- overlays.nix
+++ overlays.nix
@@ -1,5 +1,10 @@
 final: prev:
 
+let
+  removeGnomeOnlineAccounts = builtins.filter (e: e != prev.gnome-online-accounts);
+  removeGtk2 = builtins.filter (e: e != prev.gtk2);
+in
+
 {
   # Include the `--print-build-logs` flag when calling `nix build`.
   nixpkgs-review = prev.nixpkgs-review.overrideAttrs (prevAttrs: {
@@ -10,20 +15,26 @@ final: prev:
 
   # Enable building Evolution Data Server without Gnome Online Accounts (GOA)
   evolution-data-server = prev.evolution-data-server.overrideAttrs (prevAttrs: {
-    buildInputs = builtins.filter (e: e != prev.gnome-online-accounts) prevAttrs.buildInputs;
+    buildInputs = removeGnomeOnlineAccounts prevAttrs.buildInputs;
     cmakeFlags = [ "-DENABLE_GOA=OFF" ] ++ prevAttrs.cmakeFlags;
   });
 
   ibus = prev.ibus.overrideAttrs (prevAttrs: {
-    buildInputs = builtins.filter (e: e != prev.gtk2) prevAttrs.buildInputs;
+    buildInputs = removeGtk2 prevAttrs.buildInputs;
     configureFlags = [ "--disable-gtk2" ] ++ prevAttrs.configureFlags;
   });
 
   gnome = prev.gnome.overrideScope' (gnome-final: gnome-prev: {
     # Enable building Gnome Control Center without Gnome Online Accounts (GOA)
     gnome-control-center = gnome-prev.gnome-control-center.overrideAttrs (prevAttrs: {
-      buildInputs = builtins.filter (e: e != prev.gnome-online-accounts) prevAttrs.buildInputs;
+      buildInputs = removeGnomeOnlineAccounts prevAttrs.buildInputs;
       patches = prevAttrs.patches ++ [ ./remove-online-accounts-from-gnome-control-center.patch ];
+    });
+
+    # Enable building GNOME VFS without Gnome Online Accounts (GOA)
+    gvfs = gnome-prev.gvfs.overrideAttrs (prevAttrs: {
+      buildInputs = removeGnomeOnlineAccounts prevAttrs.buildInputs;
+      mesonFlags = prevAttrs.mesonFlags ++ [ "-Dgoa=false" "-Dgoogle=false" ];
     });
   });
 
