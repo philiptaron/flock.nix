0bec79dedfebd3e0264064888e172cda2503fd2a
Philip Taron <philip.taron@gmail.com>
2024-02-26 08:36:03 -0800

    Fix build break after nixos/nixpkgs#282377 was merged
    The code is now simpler and more declarative; it's worth the breakage.

--- sound.nix
+++ sound.nix
@@ -7,57 +7,52 @@
   specialArgs,
 }:
 
-let
-  json = pkgs.formats.json { };
-  pw_rnnoise_config = {
-    "context.modules" = [
-      {
-        "name" = "libpipewire-module-filter-chain";
-        "args" = {
-          "node.description" = "Noise Canceling source";
-          "media.name" = "Noise Canceling source";
-          "filter.graph" = {
-            "nodes" = [
-              {
-                "type" = "ladspa";
-                "name" = "rnnoise";
-                "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-                "label" = "noise_suppressor_stereo";
-                "control" = {
-                  "VAD Threshold (%)" = 50.0;
-                };
-              }
-            ];
-          };
-          "audio.position" = [
-            "FL"
-            "FR"
-          ];
-          "capture.props" = {
-            "node.name" = "effect_input.rnnoise";
-            "node.passive" = true;
-          };
-          "playback.props" = {
-            "node.name" = "effect_output.rnnoise";
-            "media.class" = "Audio/Source";
-          };
-        };
-      }
-    ];
-  };
-in
 {
   # Enable sound with pipewire and Bluetooth
+  hardware.pulseaudio.enable = false;
   services.pipewire.enable = true;
   services.pipewire.alsa.enable = true;
   services.pipewire.pulse.enable = true;
-  hardware.pulseaudio.enable = false;
+  services.pipewire.extraConfig.pipewire = {
+    "99-input-denoising.conf" = {
+      "context.modules" = [
+        {
+          "name" = "libpipewire-module-filter-chain";
+          "args" = {
+            "node.description" = "Noise Canceling source";
+            "media.name" = "Noise Canceling source";
+            "filter.graph" = {
+              "nodes" = [
+                {
+                  "type" = "ladspa";
+                  "name" = "rnnoise";
+                  "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                  "label" = "noise_suppressor_stereo";
+                  "control" = {
+                    "VAD Threshold (%)" = 50.0;
+                  };
+                }
+              ];
+            };
+            "audio.position" = [
+              "FL"
+              "FR"
+            ];
+            "capture.props" = {
+              "node.name" = "effect_input.rnnoise";
+              "node.passive" = true;
+            };
+            "playback.props" = {
+              "node.name" = "effect_output.rnnoise";
+              "media.class" = "Audio/Source";
+            };
+          };
+        }
+      ];
+    };
+  };
 
   # RealtimeKit service hands out realtime scheduling priority to user processes on demand.
   # pipewire and wireplumber services use this to acquire realtime priority.
   security.rtkit.enable = true;
-
-  environment.etc."pipewire/pipewire.conf.d/99-input-denoising.conf" = {
-    source = json.generate "99-input-denoising.conf" pw_rnnoise_config;
-  };
 }
