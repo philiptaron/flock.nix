c6bb697fd198ddfadc89b4a087260b00cb0d4102
Philip Taron <philip.taron@gmail.com>
2024-03-11 20:53:15 -0700

    Remove the nix.conf options dynamic-derivations, dynamic-derivations, and repl-flake

--- nix.nix
+++ nix.nix
@@ -19,23 +19,6 @@
     # Enable flakes. See the manual entry for nix flake for details.
     # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake
     "flakes"
-
-    # Allow the use of a few things related to dynamic derivations:
-    #  * "text hashing" derivation outputs, so we can build .drv files.
-    #  *  dependencies in derivations on the outputs of derivations that are themselves
-    #     derivations outputs.
-    # https://nixos.org/manual/nix/unstable/contributing/experimental-features#xp-feature-dynamic-derivations
-    "dynamic-derivations"
-
-    # Allow derivations to be content-addressed in order to prevent rebuilds when changes to the
-    # derivation do not result in changes to the derivation's output.
-    # https://nixos.org/manual/nix/unstable/language/advanced-attributes#adv-attr-__contentAddressed
-    "ca-derivations"
-
-    # Allow passing installables to `nix repl`, making its interface consistent with the other
-    # experimental commands.
-    # https://nixos.org/manual/nix/unstable/contributing/experimental-features#xp-feature-repl-flake
-    "repl-flake"
   ];
 
   environment.systemPackages = with pkgs; [
