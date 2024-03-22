e9569d607af51f16ae1e62272c09e8373dc62da8
Philip Taron <philip.taron@gmail.com>
2024-03-22 13:56:38 -0700

    Revert "Try wayland"
    This reverts commit 0f6e1c7e5b26787c8c61109081e32b00fb8e6074.
    
    As per usual, on this system, it just doesn't work.

--- hardware.nix
+++ hardware.nix
@@ -21,15 +21,16 @@
 
   # Use the latest NVIDIA out-of-tree drives.
   # See https://www.nvidia.com/en-us/drivers/unix/linux-amd64-display-archive/
+  hardware.nvidia.open = false;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production.override {
     disable32Bit = true;
   };
   boot.extraModulePackages = [ config.hardware.nvidia.package ];
 
   # The zone of "Are we Wayland yet?" with the answer "mostly not".
-  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.modesetting.enable = false;
   services.xserver.videoDrivers = [ "nvidia" ];
-  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.wayland = false;
 
   # These settings don't do anything right now. They're correct with respect to zebul, though.
   hardware.nvidia.prime.nvidiaBusId = "PCI:1:0:0";
