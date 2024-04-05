18952a7dd933a781ee321821f6da454871980cd1
Philip Taron <philip.taron@gmail.com>
2024-04-05 10:16:45 -0700

    flake.nix: spring cleaning, removing most inputs
    Flake lock file updates:
    
    • Removed input 'agenix'
    • Removed input 'agenix/darwin'
    • Removed input 'agenix/home-manager'
    • Removed input 'agenix/nixpkgs'
    • Removed input 'agenix/systems'
    • Removed input 'empty'
    • Removed input 'fh'
    • Removed input 'fh/fenix'
    • Removed input 'fh/fenix/nixpkgs'
    • Removed input 'fh/fenix/rust-analyzer-src'
    • Removed input 'fh/flake-compat'
    • Removed input 'fh/naersk'
    • Removed input 'fh/naersk/nixpkgs'
    • Removed input 'fh/nixpkgs'
    • Removed input 'llama-cpp'
    • Removed input 'llama-cpp/flake-parts'
    • Removed input 'llama-cpp/flake-parts/nixpkgs-lib'
    • Removed input 'llama-cpp/nixpkgs'
    • Removed input 'nix-darwin'
    • Removed input 'nix-darwin/nixpkgs'
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/0b7ea8db6cab824666730e470c02d0a5556e862f' (2024-04-05)
      → 'github:nixos/nixpkgs/4e317d53e6d54ef7a3738310970bae286267ef9d' (2024-04-05)
    • Removed input 'nurl'
    • Removed input 'nurl/nixpkgs'

--- flake.nix
+++ flake.nix
@@ -1,46 +1,14 @@
 {
-  description = "Philip Taron's configuration(s)";
+  description = "Philip Taron's flock of Nix configuration(s)";
   nixConfig.commit-lockfile-summary = "flake.nix: update the lockfile";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-
-    # Empty flake for making complex flake dependencies stop dead.
-    empty.url = "path:./empty.nix";
-    empty.flake = false;
-
-    agenix.url = "github:ryantm/agenix";
-    agenix.inputs.nixpkgs.follows = "nixpkgs";
-    agenix.inputs.darwin.follows = "nix-darwin";
-    agenix.inputs.home-manager.follows = "empty";
-
-    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*.tar.gz";
-    fh.inputs.nixpkgs.follows = "nixpkgs";
-
-    llama-cpp.url = "github:ggerganov/llama.cpp";
-    llama-cpp.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    nurl.url = "github:nix-community/nurl";
-    nurl.inputs.nixpkgs.follows = "nixpkgs";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      nix-darwin,
-      ...
-    }:
+    { self, nixpkgs }:
     let
       overlays = {
         default = import ./overlays.nix;
-        agenix = inputs.agenix.overlays.default;
-        fh = inputs.fh.overlays.default;
-        nurl = inputs.nurl.overlays.default;
-        llama-cpp = inputs.llama-cpp.overlays.default;
       };
 
       mkConfig = system: {
@@ -58,21 +26,13 @@
       aarch64-darwin = import nixpkgs (mkConfig "aarch64-darwin");
       aarch64-linux = import nixpkgs (mkConfig "aarch64-linux");
       nixpkgsConnection = {
-        nix.registry.nixpkgs.flake = inputs.nixpkgs;
+        nix.registry.nixpkgs.flake = nixpkgs;
       };
     in
     {
       inherit overlays;
 
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
-
-      darwinConfigurations.vesper = nix-darwin.lib.darwinSystem {
-        pkgs = aarch64-darwin;
-        modules = [
-          { networking.hostName = "vesper"; }
-          nixpkgsConnection
-        ];
-      };
 
       nixosConfigurations.zebul = nixpkgs.lib.nixosSystem {
         pkgs = x86_64-linux;
