cfa9f45a223c89132f9898959178f0f8d726467e
Philip Taron <philip.taron@gmail.com>
2024-05-03 09:17:19 -0700

    GNOME 46 has landed, meaning the patches to disable GNOME Online Accounts aren't right any more.

--- overlays.nix
+++ overlays.nix
@@ -33,35 +33,37 @@ in
     patches = (prevAttrs.patches or [ ]) ++ [ patches/nixpkgs-review/print-build-logs.patch ];
   });
 
-  # Enable building Evolution Data Server without Gnome Online Accounts (GOA)
-  evolution-data-server = prev.evolution-data-server.overrideAttrs (prevAttrs: {
-    buildInputs = removeGnomeOnlineAccounts "evolution-data-server" prevAttrs.buildInputs;
-    cmakeFlags = [ "-DENABLE_GOA=OFF" ] ++ prevAttrs.cmakeFlags;
-  });
+  disable-for-gnome-46 = {
+    # Enable building Evolution Data Server without Gnome Online Accounts (GOA)
+    evolution-data-server = prev.evolution-data-server.overrideAttrs (prevAttrs: {
+      buildInputs = removeGnomeOnlineAccounts "evolution-data-server" prevAttrs.buildInputs;
+      cmakeFlags = [ "-DENABLE_GOA=OFF" ] ++ prevAttrs.cmakeFlags;
+    });
 
-  gnome = prev.gnome.overrideScope (
-    gnome-final: gnome-prev: {
-      # Enable building Gnome Control Center without Gnome Online Accounts (GOA)
-      gnome-control-center = gnome-prev.gnome-control-center.overrideAttrs (prevAttrs: {
-        buildInputs = removeGnomeOnlineAccounts "gnome-control-center" prevAttrs.buildInputs;
-        patches = prevAttrs.patches ++ [ patches/gnome-control-center/remove-gnome-online-accounts.patch ];
-      });
+    gnome = prev.gnome.overrideScope (
+      gnome-final: gnome-prev: {
+        # Enable building Gnome Control Center without Gnome Online Accounts (GOA)
+        gnome-control-center = gnome-prev.gnome-control-center.overrideAttrs (prevAttrs: {
+          buildInputs = removeGnomeOnlineAccounts "gnome-control-center" prevAttrs.buildInputs;
+          patches = prevAttrs.patches ++ [ patches/gnome-control-center/remove-gnome-online-accounts.patch ];
+        });
 
-      # Enable building GNOME VFS without Gnome Online Accounts (GOA)
-      gvfs = gnome-prev.gvfs.overrideAttrs (prevAttrs: {
-        buildInputs = removeGnomeOnlineAccounts "gvfs" prevAttrs.buildInputs;
-        mesonFlags = prevAttrs.mesonFlags ++ [
-          "-Dgoa=false"
-          "-Dgoogle=false"
-        ];
-      });
-    }
-  );
+        # Enable building GNOME VFS without Gnome Online Accounts (GOA)
+        gvfs = gnome-prev.gvfs.overrideAttrs (prevAttrs: {
+          buildInputs = removeGnomeOnlineAccounts "gvfs" prevAttrs.buildInputs;
+          mesonFlags = prevAttrs.mesonFlags ++ [
+            "-Dgoa=false"
+            "-Dgoogle=false"
+          ];
+        });
+      }
+    );
 
-  libgdata = prev.libgdata.overrideAttrs (prevAttrs: {
-    propagatedBuildInputs = removeGnomeOnlineAccounts "libgdata" prevAttrs.propagatedBuildInputs;
-    mesonFlags = prevAttrs.mesonFlags ++ [ "-Dgoa=disabled" ];
-  });
+    libgdata = prev.libgdata.overrideAttrs (prevAttrs: {
+      propagatedBuildInputs = removeGnomeOnlineAccounts "libgdata" prevAttrs.propagatedBuildInputs;
+      mesonFlags = prevAttrs.mesonFlags ++ [ "-Dgoa=disabled" ];
+    });
+  };
 
   # On zebul, we use CUDA 12.3
   cudaPackages = final.cudaPackages_12_3;
