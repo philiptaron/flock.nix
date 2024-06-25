753d7fd9c35746e703f959cd7f868968de2d5652
Philip Taron <philip.taron@gmail.com>
2024-06-25 16:40:12 -0700

    vesper.nix: use with statement to grab packages

--- vesper.nix
+++ vesper.nix
@@ -1,34 +1,11 @@
 # This is a poor man's `nix-darwin`.
 # Install with `nix profile install github:philiptaron/flock.nix#vesper`
 # You might need to clear out all other installed items.
-{
-  symlinkJoin,
-
-  bash-completion,
-  bashInteractive,
-  bat,
-  cacert,
-  fd,
-  findutils,
-  gh,
-  git,
-  h,
-  jq,
-  man,
-  mosh,
-  nix-output-monitor,
-  nixpkgs-review,
-  nixVersions,
-  openssh,
-  pstree,
-  ripgrep,
-  vim,
-  uutils-coreutils-noprefix,
-}:
+{ pkgs, symlinkJoin }:
 
 symlinkJoin {
   name = "vesper";
-  paths = [
+  paths = with pkgs; [
     bash-completion
     bashInteractive
     bat
