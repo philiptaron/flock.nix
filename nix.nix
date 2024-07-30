7c21f46d5457126bb08526115f94f3f08b105ea5
Philip Taron <philip.taron@gmail.com>
2024-07-30 16:18:24 -0700

    Let's try out a version of Nix that sets NIX_TRACE_VERBOSE in derivations

--- nix.nix
+++ nix.nix
@@ -8,8 +8,10 @@
 }:
 
 {
-  # We'll live dangerously.
-  nix.package = pkgs.nixVersions.latest;
+  # We'll live dangerously. And we'll apply a patch that lets us do verbose tracing.
+  nix.package = pkgs.nixVersions.latest.overrideAttrs (prevAttrs: {
+    patches = (prevAttrs.patches or [ ]) ++ [ ./patches/nix/0001-WIP-env-NIX_TRACE_VERBOSE.patch ];
+  });
 
   # Let's try having a small set of build machines.
   nix.distributedBuilds = true;
