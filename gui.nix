ab8e91b9ed901c3df88a75e441ceb312200910af
Philip Taron <philip.taron@gmail.com>
2024-09-25 07:59:37 -0700

    Revert "Maybe it was XWayland"
    This reverts commit 807673119b4b1bfeef18ccae72ae364502e77cda.

--- gui.nix
+++ gui.nix
@@ -9,6 +9,7 @@
 
 {
   services.desktopManager.cosmic.enable = true;
+  services.desktopManager.cosmic.xwayland.enable = false;
   services.displayManager.cosmic-greeter.enable = true;
 
   # Remove a few things
@@ -17,6 +18,9 @@
     cosmic-edit
     cosmic-term
   ];
+
+  # Enable discovery of stuff. We'll try to get a smaller hammer over time.
+  environment.pathsToLink = [ "/share" ];
 
   # Make the fonts look better.
   fonts = {
