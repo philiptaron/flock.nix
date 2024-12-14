19554fb025e68fb18b3b9d57e81854e5f446a987
Philip Taron <philip.taron@gmail.com>
2024-12-14 14:34:45 -0800

    programs: remove flatpak
    We're going more minimal for a second.

--- programs.nix
+++ programs.nix
@@ -248,9 +248,6 @@ in
     llama-cpp
   ];
 
-  # Use flatpak to run Zoom. It's more compatible than the NixOS version.
-  services.flatpak.enable = true;
-
   users.users.philip.packages = [
     # `alacritty` is a cross-platform, GPU-accelerated terminal emulator.
     # https://github.com/alacritty/alacritty
