3efc4cb8d3d79ff2ee084ec3d05d600f8c5eb82f
Philip Taron <philip.taron@gmail.com>
2025-01-07 09:51:53 -0800

    programs: downgrade zoom-us to 6.2.11.5069

--- programs.nix
+++ programs.nix
@@ -279,7 +279,13 @@ in
 
     # Zoom, again.
     # https://qumulo.zoom.us
-    pkgs.zoom-us
+    (pkgs.zoom-us.overrideAttrs {
+      version = "6.2.11.5069";
+      src = pkgs.fetchurl {
+        url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
+        hash = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
+      };
+    })
   ];
 
   systemd.user.tmpfiles.users.philip.rules = [
