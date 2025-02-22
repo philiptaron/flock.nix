3a41a63eacadc186487e8e8614481768a8449836
Philip Taron <philip.taron@gmail.com>
2025-02-21 18:11:14 -0800

    Try it with an overlay

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Philip Taron's flock of Nix configuration(s)";
   nixConfig.commit-lockfile-summary = "flake.nix: update the lockfile";
 
-  inputs.nixpkgs.url = "github:philiptaron/nixpkgs/busybox-sandbox";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   outputs =
     { self, nixpkgs, ... }:
--- overlays.nix
+++ overlays.nix
@@ -32,4 +32,36 @@ in
 
   # On zebul, we use a driver that can do CUDA 12.6, but this is the latest in nixpkgs.
   cudaPackages = final.cudaPackages_12_4;
+
+  # Minimal shell for use as basic /bin/sh in sandbox builds
+  busybox-sandbox-shell = final.busybox.override {
+    useMusl = true;
+    enableStatic = true;
+    enableMinimal = true;
+
+    extraConfig = ''
+      CONFIG_FEATURE_FANCY_ECHO y
+      CONFIG_FEATURE_SH_MATH y
+      CONFIG_FEATURE_SH_MATH_64 y
+      CONFIG_FEATURE_TEST_64 y
+
+      CONFIG_MKDIR y
+      CONFIG_LN y
+      CONFIG_MV y
+      CONFIG_CP y
+
+      CONFIG_ASH y
+      CONFIG_ASH_OPTIMIZE_FOR_SIZE y
+
+      CONFIG_ASH_ALIAS y
+      CONFIG_ASH_BASH_COMPAT y
+      CONFIG_ASH_CMDCMD y
+      CONFIG_ASH_ECHO y
+      CONFIG_ASH_GETOPTS y
+      CONFIG_ASH_INTERNAL_GLOB y
+      CONFIG_ASH_JOB_CONTROL y
+      CONFIG_ASH_PRINTF y
+      CONFIG_ASH_TEST y
+    '';
+  };
 }
