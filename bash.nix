d3698e0728f56eadba3a79159dbdd8b2654b22d1
Philip Taron <philip.taron@gmail.com>
2025-03-03 13:25:37 -0800

    bash: turn off programs.bash.enableLsColors, and turn on promptline

--- bash.nix
+++ bash.nix
@@ -22,6 +22,14 @@ in
   # Give a bashrc that's worth a damn.
   programs.bash.interactiveShellInit = "source ${bashrc}";
 
+  # Don't turn on colors through `LS_COLORS` environment variable.
+  programs.bash.enableLsColors = false;
+
+  # Use <https://github.com/edkolev/promptline.vim> to make a prompt.
+  programs.bash.promptPluginInit = ''
+    source ${dotfiles/bash/promptline.sh}
+  '';
+
   # Set up inputrc to be my custom one.
   environment.etc.inputrc.source = dotfiles/readline/inputrc;
 }
