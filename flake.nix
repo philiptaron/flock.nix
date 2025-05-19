33f6461c919ab1534112c64e8618c18ff71307c9
Philip Taron <philip.taron@gmail.com>
2025-05-19 16:41:20 -0700

    flake.nix: update the lockfile to go back to NixOS master branch
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/45122b3639e74ad42cc7c1c471444a8fe7dfac6f?narHash=sha256-emSGkcEfWSS3M7Cu6d1mxoMF%2BtW0HKsSetbB6oC0QVM%3D' (2025-05-06)
      → 'github:NixOS/nixpkgs/359c442b7d1f6229c1dc978116d32d6c07fe8440?narHash=sha256-TvJjbLlQ5aAHS3ZdP8mztNs28cMGWdT3J9g/6li3/4I%3D' (2025-05-19)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Philip Taron's flock of Nix configuration(s)";
   nixConfig.commit-lockfile-summary = "flake.nix: update the lockfile";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=pull/386514/merge";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
   outputs =
     { self, nixpkgs, ... }:
