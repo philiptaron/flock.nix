d6c7da958a150c06b117bdf10e3b54aa66a9d855
Philip Taron <philip.taron@gmail.com>
2025-07-18 09:33:25 -0700

    programs: allow htop derivation to continue doing whatever it needs to do in postInstall

--- programs.nix
+++ programs.nix
@@ -24,11 +24,13 @@ in
   # https://htop.dev/
   programs.htop = {
     enable = true;
-    package = pkgs.htop.overrideAttrs (prev: {
+    package = pkgs.htop.overrideAttrs (prevAttrs: {
       # Remove the .desktop icon; no need to launch htop from Gnome.
-      postInstall = ''
-        rm -rf $out/share/{applications,icons,pixmaps}
-      '';
+      postInstall =
+        (prevAttrs.postInstall or "")
+        + ''
+          rm -rf $out/share/{applications,icons,pixmaps}
+        '';
     });
   };
 
