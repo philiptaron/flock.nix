64a1aae2ea5277f1a9741c2b68dfd44bc56b6229
Philip Taron <philip.taron@gmail.com>
2025-12-02 14:43:11 -0800

    networking: turn on the firewall

--- network.nix
+++ network.nix
@@ -1,36 +1,39 @@
 { pkgs, ... }:
 
 {
-  # Enable networking through systemd-networkd; don't use the built-in NixOS modules.
-  networking.useDHCP = false;
-  networking.dhcpcd.enable = false;
-  networking.useNetworkd = false;
-  systemd.network.enable = true;
+  systemd.network = {
+    # Enable networking through systemd-networkd; don't use the built-in NixOS modules.
+    enable = true;
 
-  # Turn off the firewall altogether.
-  networking.firewall.enable = false;
+    # Adjust wlan0 to have the highest MTU that this device offers.
+    links = {
+      "79-wlan0" = {
+        matchConfig.OriginalName = "wlan0";
+        matchConfig.Type = "wlan";
+        linkConfig.NamePolicy = "keep kernel";
+        linkConfig.MTUBytes = "2304";
+      };
+    };
+
+    # Use DHCP to configure wlan station devices.
+    networks = {
+      "ether-uses-dhcp" = {
+        matchConfig.Type = "ether";
+        networkConfig.DHCP = "yes";
+        dhcpV4Config.UseMTU = true;
+      };
+    };
+  };
+
+  networking = {
+    useDHCP = false;
+    useNetworkd = true;
+    firewall.enable = true;
+    nftables.enable = true;
+  };
 
   # Turn on verbose logging for systemd-networkd.
   systemd.services.systemd-networkd.serviceConfig.Environment = "SYSTEMD_LOG_LEVEL=debug";
-
-  # Adjust wlan0 to have the highest MTU that this device offers.
-  systemd.network.links = {
-    "79-wlan0" = {
-      matchConfig.OriginalName = "wlan0";
-      matchConfig.Type = "wlan";
-      linkConfig.NamePolicy = "keep kernel";
-      linkConfig.MTUBytes = "2304";
-    };
-  };
-
-  # Use DHCP to configure wlan station devices.
-  systemd.network.networks = {
-    "ether-uses-dhcp" = {
-      matchConfig.Type = "ether";
-      networkConfig.DHCP = "yes";
-      dhcpV4Config.UseMTU = true;
-    };
-  };
 
   # `wireshark` is a network packet tracing application
   # https://www.wireshark.org/
