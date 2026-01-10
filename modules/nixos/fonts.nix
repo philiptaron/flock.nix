db67c371eb368d36c90ef045cc6576992f0ae2ab
Philip Taron <philip.taron@gmail.com>
2026-01-10 08:41:20 -0800

    fonts: add TX-02 font
    Fetch the base64-encoded zip from a gist, decode and unzip it in
    postFetch to install the font files.

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,10 +1,28 @@
 { config, pkgs, ... }:
 
+let
+  TX-02 = pkgs.fetchurl {
+    url = "https://gist.githubusercontent.com/philiptaron/31d7692feb76e948e245ed3d4e40a3ba/raw/f7b4714a850ad07c46878e51467f41e34a578569/TX-02.zip.b64";
+    hash = "sha256-0yyc4wJr+FZZ0CAVFBl5cKmMDFkCR7FbNPlT5zMEkz0=";
+
+    nativeBuildInputs = [ pkgs.unzip ];
+
+    downloadToTemp = true;
+
+    postFetch = ''
+      mkdir -p $out/share/fonts/truetype
+      base64 -d $downloadedFile > fonts.zip
+      unzip -j fonts.zip -d $out/share/fonts/truetype
+    '';
+  };
+in
+
 # We're all in on Google's `noto` (NO TOfu) fonts.
 {
   fonts = {
     enableDefaultPackages = false;
     packages = [
+      TX-02
       pkgs.noto-fonts
       pkgs.noto-fonts-cjk-sans
       pkgs.noto-fonts-cjk-serif
