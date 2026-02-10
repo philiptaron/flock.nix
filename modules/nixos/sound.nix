dd8cfe2323a87e6979dcc0f74894eae664cf09b9
Philip Taron <philip.taron@gmail.com>
2026-02-10 10:45:21 -0800

    sound.nix: remove rnnoise noise cancellation filter

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -1,5 +1,3 @@
-{ pkgs, ... }:
-
 {
   # Enable sound with pipewire and Bluetooth
   services.pulseaudio.enable = false;
@@ -9,45 +7,6 @@
 
   # Turn off speech-dispatcher.
   services.speechd.enable = false;
-
-  services.pipewire.extraConfig.pipewire = {
-    "99-input-denoising.conf" = {
-      "context.modules" = [
-        {
-          "name" = "libpipewire-module-filter-chain";
-          "args" = {
-            "node.description" = "Noise Canceling source";
-            "media.name" = "Noise Canceling source";
-            "filter.graph" = {
-              "nodes" = [
-                {
-                  "type" = "ladspa";
-                  "name" = "rnnoise";
-                  "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-                  "label" = "noise_suppressor_stereo";
-                  "control" = {
-                    "VAD Threshold (%)" = 50.0;
-                  };
-                }
-              ];
-            };
-            "audio.position" = [
-              "FL"
-              "FR"
-            ];
-            "capture.props" = {
-              "node.name" = "effect_input.rnnoise";
-              "node.passive" = true;
-            };
-            "playback.props" = {
-              "node.name" = "effect_output.rnnoise";
-              "media.class" = "Audio/Source";
-            };
-          };
-        }
-      ];
-    };
-  };
 
   # RealtimeKit service hands out realtime scheduling priority to user processes on demand.
   # pipewire and wireplumber services use this to acquire realtime priority.
